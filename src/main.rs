0628c7ad405d89e1addc74e96be466819ef2c0d0
th0rex <c0untlizzi@gmail.com>
2018-07-29 15:25:44 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,273 @@
+#![deny(bare_trait_objects)]
+#![feature(try_from)]
+
+use std::convert::{TryFrom, TryInto};
+use std::env;
+use std::ffi::CStr;
+use std::fmt::Write;
+use std::num::ParseIntError;
+
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+use zydis::gen::{
+    ZYDIS_ADDRESS_WIDTH_32, ZYDIS_ADDRESS_WIDTH_64, ZYDIS_MACHINE_MODE_LONG_64,
+    ZYDIS_MACHINE_MODE_LONG_COMPAT_32, ZYDIS_ADDR_FORMAT_ABSOLUTE,
+    ZYDIS_ADDR_FORMAT_RELATIVE_SIGNED, ZYDIS_ADDR_FORMAT_RELATIVE_UNSIGNED,
+    ZYDIS_DISP_FORMAT_HEX_SIGNED, ZYDIS_DISP_FORMAT_HEX_UNSIGNED, ZYDIS_FORMATTER_STYLE_INTEL,
+    ZYDIS_IMM_FORMAT_HEX_AUTO, ZYDIS_IMM_FORMAT_HEX_SIGNED, ZYDIS_IMM_FORMAT_HEX_UNSIGNED,
+};
+use zydis::gen::{ZydisAddressFormat, ZydisDisplacementFormat, ZydisImmediateFormat};
+use zydis::{gen::ZydisStatusCodes, Decoder, Formatter, FormatterProperty};
+
+enum Arch {
+    X86,
+    X64,
+}
+
+#[derive(Default)]
+struct Options {
+    arch: Option<Arch>,
+
+    base: Option<u64>,
+
+    uppercase: Option<bool>,
+    force_memseg: Option<bool>,
+    force_memsize: Option<bool>,
+    address_format: Option<ZydisAddressFormat>,
+    disp_format: Option<ZydisDisplacementFormat>,
+    imm_format: Option<ZydisImmediateFormat>,
+    hex_uppercase: Option<bool>,
+    hex_padding_addr: Option<u8>,
+    hex_padding_disp: Option<u8>,
+    hex_padding_imm: Option<u8>,
+}
+
+trait ParseStrRadix<T> {
+    fn parse_radix(&self, base: u32) -> Result<T, ParseIntError>;
+}
+
+impl ParseStrRadix<u64> for str {
+    fn parse_radix(&self, base: u32) -> Result<u64, ParseIntError> {
+        u64::from_str_radix(self, base)
+    }
+}
+
+impl ParseStrRadix<u8> for str {
+    fn parse_radix(&self, base: u32) -> Result<u8, ParseIntError> {
+        u8::from_str_radix(self, base)
+    }
+}
+
+macro_rules! parse_number {
+    ($v:expr, $o:expr, $s:expr, $t:expr) => {
+        let len = $s.len();
+        if $o.len() > len && &$o[..len] == $s {
+            let tmp = &$o[len..];
+            let (offset, base) = if tmp.find("0x").is_some() {
+                (2, 16)
+            } else {
+                (0, 10)
+            };
+
+            $v = (&tmp[offset..]).parse_radix(base).ok();
+        }
+    };
+}
+
+impl Options {
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    fn parse_option(&mut self, opt: &str) {
+        match opt {
+            "x86"                         => self.arch           = Some(Arch::X86),
+            "x64"                         => self.arch           = Some(Arch::X64),
+            "uppercase=true"              => self.uppercase      = Some(true),
+            "uppercase=false"             => self.uppercase      = Some(false),
+            "force_memseg=true"           => self.force_memseg   = Some(true),
+            "force_memseg=false"          => self.force_memseg   = Some(false),
+            "force_memsize=true"          => self.force_memsize  = Some(true),
+            "force_memsize=false"         => self.force_memsize  = Some(false),
+            "address_format=absolute"     => self.address_format = Some(ZYDIS_ADDR_FORMAT_ABSOLUTE),
+            "address_format=unsigned_rel" => self.address_format = Some(ZYDIS_ADDR_FORMAT_RELATIVE_UNSIGNED),
+            "address_format=signed_rel"   => self.address_format = Some(ZYDIS_ADDR_FORMAT_RELATIVE_SIGNED),
+            "disp_format=signed"          => self.address_format = Some(ZYDIS_DISP_FORMAT_HEX_SIGNED),
+            "disp_format=unsigned"        => self.address_format = Some(ZYDIS_DISP_FORMAT_HEX_UNSIGNED),
+            "imm_format=auto"             => self.imm_format     = Some(ZYDIS_IMM_FORMAT_HEX_AUTO),
+            "imm_format=signed"           => self.imm_format     = Some(ZYDIS_IMM_FORMAT_HEX_SIGNED),
+            "imm_format=unsigned"         => self.imm_format     = Some(ZYDIS_IMM_FORMAT_HEX_UNSIGNED),
+            _                             => {
+                parse_number!(self.base,             opt, "base=",     u64);
+                parse_number!(self.hex_padding_addr, opt, "pad_addr=", u8);
+                parse_number!(self.hex_padding_disp, opt, "pad_disp=", u8);
+                parse_number!(self.hex_padding_imm,  opt, "pad_imm=",  u8);
+            },
+        }
+    }
+}
+
+impl<'a> TryFrom<Options> for (u64, Formatter<'a>, Decoder) {
+    type Error = ZydisStatusCodes;
+
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    fn try_from(options: Options) -> Result<Self, Self::Error> {
+        let mut formatter = Formatter::new(ZYDIS_FORMATTER_STYLE_INTEL)?;
+
+        let decoder = match options.arch.unwrap_or(Arch::X86) {
+            Arch::X86 => Decoder::new(ZYDIS_MACHINE_MODE_LONG_COMPAT_32, ZYDIS_ADDRESS_WIDTH_32)?,
+            Arch::X64 => Decoder::new(ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_ADDRESS_WIDTH_64)?,
+        };
+
+        formatter.set_property(FormatterProperty::Uppercase(options.uppercase.unwrap_or(false)))?;
+        formatter.set_property(FormatterProperty::ForceMemseg(options.force_memseg.unwrap_or(false)))?;
+        formatter.set_property(FormatterProperty::ForceMemsize(options.force_memsize.unwrap_or(false)))?;
+        formatter.set_property(FormatterProperty::AddressFormat(options.address_format.unwrap_or(ZYDIS_ADDR_FORMAT_ABSOLUTE)))?;
+        formatter.set_property(FormatterProperty::DispFormat(options.disp_format.unwrap_or(ZYDIS_DISP_FORMAT_HEX_UNSIGNED)))?;
+        formatter.set_property(FormatterProperty::ImmFormat(options.imm_format.unwrap_or(ZYDIS_IMM_FORMAT_HEX_AUTO)))?;
+        formatter.set_property(FormatterProperty::HexUppercase(options.hex_uppercase.unwrap_or(true)))?;
+        formatter.set_property(FormatterProperty::HexPaddingAddr(options.hex_padding_addr.unwrap_or(2)))?;
+        formatter.set_property(FormatterProperty::HexPaddingDisp(options.hex_padding_disp.unwrap_or(2)))?;
+        formatter.set_property(FormatterProperty::HexPaddingImm(options.hex_padding_imm.unwrap_or(2)))?;
+
+        let base = options.base.unwrap_or(0);
+
+        Ok((base, formatter, decoder))
+    }
+}
+
+fn decode_hex(mut b: u8) -> u8 {
+    b -= b'0';
+    if b > 9 {
+        b - 7
+    } else {
+        b
+    }
+}
+
+fn is_hex_digit(b: u8) -> bool {
+    (b as char).is_digit(16)
+}
+
+fn decode_bytes_into(hex: &[u8], bytes: &mut Vec<u8>) {
+    let mut tmp = 0;
+
+    for (i, &b) in hex.iter().enumerate() {
+        if b == b'0' && i + 1 < hex.len() && hex[i + 1] == b'x' {
+            continue;
+        }
+
+        if is_hex_digit(b) {
+            let b = b.to_ascii_uppercase();
+            if tmp != 0 {
+                bytes.push((tmp << 4) | decode_hex(b));
+
+                tmp = 0;
+            } else {
+                tmp = decode_hex(b);
+            }
+        }
+    }
+}
+
+struct Handler;
+
+impl Handler {
+    fn help_message(&self, msg: &Message) {
+        msg.channel_id
+            .say(
+                r#"Available commands:
+```
+!help - Help
+!dis OPTIONS <hex here> - disassemble
+   OPTIONS can be one or more of the following, seperated by a space:
+   +x86, +x64, +uppercase={true|false}, +force_memseg={true|false}, +force_memsize={true|false},
+   +address_format={absolute|unsigned_rel|signed_rel}, +disp_format={signed|unsigned}, +imm_format={auto|signed|unsigned}
+For example:
+```
+!dis +x64 \x90
+or
+!dis +x64 0x90
+or
+!dis +x64 9090
+"#,
+            )
+            .unwrap();
+    }
+
+    fn disassemble(&self, data: &str, msg: &Message) {
+        let mut bytes = Vec::with_capacity(1024);
+        let mut options = Options::default();
+
+        for thing in data.trim().split(' ') {
+            if thing.len() == 0 {
+                continue;
+            }
+
+            if &thing[..1] == "+" {
+                options.parse_option(&thing[1..]);
+            } else {
+                decode_bytes_into(thing.as_bytes(), &mut bytes);
+            }
+        }
+
+        let (base, formatter, decoder) = options.try_into().unwrap();
+
+        let mut message = String::new();
+        let mut buffer = vec![0u8; 200];
+
+        message.push_str("```x86asm\n");
+
+        for (insn, ip) in decoder.instruction_iterator(&bytes, base) {
+            unsafe { std::ptr::write_bytes(buffer.as_mut_ptr(), 0u8, 200) };
+
+            formatter
+                .format_instruction_raw(&insn, &mut buffer, None)
+                .unwrap();
+
+            let insn = unsafe { CStr::from_bytes_with_nul_unchecked(&buffer) }
+                .to_str()
+                .unwrap();
+
+            write!(message, "0x{:016X} {}\n", ip, insn).unwrap();
+        }
+
+        message.push_str("```");
+
+        if message.len() < 2000 {
+            msg.channel_id.say(message).unwrap();
+        } else {
+            msg.channel_id.say(message).unwrap();
+        }
+    }
+}
+
+impl EventHandler for Handler {
+    fn message(&self, _: Context, msg: Message) {
+        if msg.author.bot {
+            return;
+        }
+
+        let content = &msg.content;
+
+        if content.len() < 5 {
+            return;
+        }
+
+        match &content[..5] {
+            "!help" => self.help_message(&msg),
+            "!dis " => self.disassemble(&content[2..], &msg),
+            _ => {}
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected token");
+    let mut client = Client::new(&token, Handler).unwrap();
+
+    client.start().unwrap();
+}
