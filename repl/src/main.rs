68f512015c953a3fc85a11a286961e8db7cecc64
th0rex <c0untlizzi@gmail.com>
2018-07-29 23:28:16 +0200

    Add REPL

---
+++ repl/src/main.rs
@@ -0,0 +1,24 @@
+use std::io::{stdin, stdout, BufRead, Result, Write};
+
+use shared::handle_command;
+
+fn main() -> Result<()> {
+    let mut bytes = vec![];
+    let mut out = String::new();
+    let mut inp = String::new();
+
+    print!("> ");
+    stdout().flush()?;
+
+    loop {
+        inp.clear();
+
+        let stdin = stdin();
+        stdin.lock().read_line(&mut inp)?;
+
+        handle_command(&inp, &mut bytes, &mut out, None, None);
+
+        print!("{}\n> ", out);
+        stdout().flush()?;
+    }
+}
